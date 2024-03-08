676ed15091ca937de585d8da0c23399fee9489e2
Boris Willekens <158263511+BorisWillekens@users.noreply.github.com>
2024-03-08 15:50:35 +0100

    Updated Download_data.R

--- explore_data_imdb/Download_data.R
+++ explore_data_imdb/Download_data.R
@@ -6,7 +6,8 @@ download_data <- function(url, filename){
 
 url_crew <- "https://datasets.imdbws.com/title.crew.tsv.gz"
 url_ratings <- "https://datasets.imdbws.com/title.ratings.tsv.gz"
+url_name_basics <- "https://datasets.imdbws.com/name.basics.tsv.gz"
 
 download_data(url_crew, "crew")
 download_data(url_ratings, "ratings")
-
+download_data(url_name_basics, "name_basics")
